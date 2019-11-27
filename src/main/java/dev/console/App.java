09e17e55b1bbd1f50772043347999d82187bb73e
Matt34000 <matthieu-le-roy@gmx.fr>
2019-11-27 14:57:39 +0100

    commit

---
+++ src/main/java/dev/console/App.java
@@ -0,0 +1,21 @@
+package dev.console;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dev.service.AppService;
+
+public class App {
+	private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		LOG.info("Bonjour Slf4J !");
+		LOG.info("Implementation Logback");
+		
+		AppService ap = new AppService();
+		ap.executer("test3");
+		
+	}
+
+
+}
